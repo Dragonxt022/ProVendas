049727f6e60b568e136b4b51b8356026c37612fc
Dragonxt022 <pissinatti2019@gmail.com>
2024-11-12 15:38:30 -0400

    Correções esteticas em configuração

--- provendas/configuracoes/forms.py
+++ provendas/configuracoes/forms.py
@@ -4,8 +4,12 @@ from .models import Configuracao
 class ConfiguracaoForm(forms.ModelForm):
     class Meta:
         model = Configuracao
-        fields = ['nome_aplicacao', 'cliente_padrao', 'cor_primaria', 'cor_secundaria', 'logo_empresa']
+        fields = ['nome_aplicacao', 'cliente_padrao', 'cor_primaria', 'cor_secundaria', 'logo_empresa', 'icone_aplicacao']
         widgets = {
-            'cor_primaria': forms.TextInput(attrs={'type': 'color'}),
-            'cor_secundaria': forms.TextInput(attrs={'type': 'color'}),
+            'nome_aplicacao': forms.TextInput(attrs={'class': 'form-control'}),
+            'cliente_padrao': forms.Select(attrs={'class': 'form-control'}),
+            'cor_primaria': forms.TextInput(attrs={'type': 'color', 'class': 'form-control'}),
+            'cor_secundaria': forms.TextInput(attrs={'type': 'color', 'class': 'form-control'}),
+            'logo_empresa': forms.FileInput(attrs={'class': 'form-control'}),
+            'icone_aplicacao': forms.FileInput(attrs={'class': 'form-control'}),
         }
--- provendas/configuracoes/models.py
+++ provendas/configuracoes/models.py
@@ -1,11 +1,14 @@
+# models.py
 from django.db import models
+from provendas.utils import caminho_upload
 
 class Configuracao(models.Model):
     nome_aplicacao = models.CharField(max_length=100)
     cliente_padrao = models.ForeignKey('clientes.Cliente', on_delete=models.SET_NULL, null=True, blank=True)
-    cor_primaria = models.CharField(max_length=7, default="#ff670f")  # Exemplo de cor no formato hex
-    cor_secundaria = models.CharField(max_length=7, default="#ffd30f")
-    logo_empresa = models.ImageField(upload_to='logos/', null=True, blank=True)
-    
+    cor_primaria = models.CharField(max_length=7, default="#000000")
+    cor_secundaria = models.CharField(max_length=7, default="#FFFFFF")
+    logo_empresa = models.ImageField(upload_to=caminho_upload, null=True, blank=True)
+    icone_aplicacao = models.ImageField(upload_to=caminho_upload, null=True, blank=True)  # Novo campo para o ícone
+
     def __str__(self):
-        return "Configurações Gerais"
+        return self.nome_aplicacao
