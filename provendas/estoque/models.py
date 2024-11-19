6cc1c1aa5d075b2af399ccd619fd351009b09de2
Dragonxt022 <pissinatti2019@gmail.com>
2024-11-18 21:30:21 -0400

    varias correções

--- provendas/estoque/models.py
+++ provendas/estoque/models.py
@@ -9,10 +9,16 @@ class CategoriaProduto(models.Model):
     slug = models.SlugField(max_length=100, unique=True, blank=True)
     file = models.ImageField(upload_to=caminho_upload, null=True, blank=True)
 
-
     def save(self, *args, **kwargs):
-        if not self.slug:  # Gera o slug apenas se ele ainda não existir
-            self.slug = slugify(self.nome)
+        if not self.slug:
+            base_slug = slugify(self.nome)
+            slug = base_slug
+            counter = 1
+            # Garante que o slug seja único
+            while CategoriaProduto.objects.filter(slug=slug).exists():
+                slug = f"{base_slug}-{counter}"
+                counter += 1
+            self.slug = slug
         super().save(*args, **kwargs)
 
     def __str__(self):
