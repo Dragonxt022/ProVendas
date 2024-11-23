b05e1a0b8f6fe3b62befdd7159f0dd2597ed02a6
Dragonxt022 <pissinatti2019@gmail.com>
2024-11-23 16:46:20 -0400

    Add - Sistema de notificação

--- provendas/configuracoes/context_processors.py
+++ provendas/configuracoes/context_processors.py
@@ -1,9 +1,50 @@
+import subprocess
 from .models import Configuracao
 from django.utils import timezone
 from django.utils.timezone import now
 from licencas.models import LicenseKey
 from caixa.models import Caixa
+from datetime import datetime
 
+
+def get_git_commits():
+    try:
+        result = subprocess.run(['git', 'log', '--oneline', '--max-count=5'], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+        
+        if result.returncode == 0:
+            commits = result.stdout.splitlines()
+            formatted_commits = []
+            
+            # Itera sobre cada commit
+            for commit in commits:
+                commit_hash, commit_message = commit.split(" ", 1)
+                
+                # Nenhuma normalização necessária, apenas deixa a mensagem como está
+                # O Python já lida bem com caracteres especiais se os arquivos estiverem codificados corretamente
+                
+                # Obtém a data do commit
+                commit_date = subprocess.run(
+                    ['git', 'log', '--format=%cd', '--max-count=1', '--date=short', commit_hash],
+                    stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True
+                ).stdout.strip()
+                
+                # Formatar a data para 'd/m/yy' (dd/mm/yy)
+                commit_date_obj = datetime.strptime(commit_date, '%Y-%m-%d')  # Converte a string para datetime
+                formatted_date = commit_date_obj.strftime('%d/%m/%y')  # Formato no estilo BR
+                
+                formatted_commits.append({
+                    'commit_hash': commit_hash,
+                    'commit_message': commit_message,  # A mensagem já vem corretamente formatada
+                    'commit_date': formatted_date
+                })
+            
+            return formatted_commits
+        else:
+            return []
+    except Exception as e:
+        print(f"Erro ao obter commits: {e}")
+        return []
+    
 def license_days_remaining(request):
     days_remaining = 0
     # Buscar qualquer chave ativa, independentemente do usuário
@@ -30,18 +71,45 @@ def configuracoes(request):
         caixa_esta_aberto = caixa_aberto is not None
         status_caixa = caixa_aberto.status if caixa_aberto else 'N/D'
         abrir_caixa_automatico = configuracao.gerenciar_abertura_fechamento_caixa if configuracao else False
-
-        print(f"Valor de abrir_caixa_automatico: {abrir_caixa_automatico}")
-        
-
-        
-
         
     else:
         caixa_aberto = None
         caixa_esta_aberto = False
         status_caixa = 'N/D'
         abrir_caixa_automatico= None
+
+
+    # Notificações - Exemplo, ajustando para sua lógica de notificação de licenças
+    license_info = license_days_remaining(request)
+    days_remaining = license_info['days_remaining']
+
+    notificacoes = []
+    if days_remaining == -1:
+        notificacoes.append({
+            'mensagem': 'Licença expirada!',
+            'icone': 'fas fa-exclamation-triangle',
+            'cor': '#FFD43B',
+            'quantidade': 1
+        })
+    elif days_remaining <= 7:
+        notificacoes.append({
+            'mensagem': f'Licença expira em {days_remaining} dias!',
+            'icone': 'fas fa-exclamation-circle',
+            'cor': '#FF4500',
+            'quantidade': 1
+        })
+
+     # Obter commits
+    commits = get_git_commits()
+    if commits:
+        for commit in commits:
+            notificacoes.append({
+                'mensagem': f'{commit["commit_message"]}',
+                'data': commit['commit_date'],  # Usando a data formatada
+                'icone': 'fas fa-code-branch',
+                'cor': '#28A745',
+                'quantidade': 1
+            })
 
     return {
         'nome_aplicacao': configuracao.nome_aplicacao,
@@ -55,4 +123,5 @@ def configuracoes(request):
         'caixa_esta_aberto': caixa_esta_aberto,
         'abrir_caixa_automatico': abrir_caixa_automatico,
         'status_caixa': status_caixa,
+        'notificacoes': notificacoes,
     }
